7f55324892a4bd76a7116d0894b180576eece8cb
Raju Vimalraj <e0146859@u.nus.edu>
2017-09-24 14:32:25 +0800

    ui initial commit
    not the pushing the complete ui for login and main screen

--- PRMS_ANDROID/app/src/main/java/sg/edu/nus/iss/phoenix/authenticate/android/ui/LoginScreen.java
+++ PRMS_ANDROID/app/src/main/java/sg/edu/nus/iss/phoenix/authenticate/android/ui/LoginScreen.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.iss.phoenix.authenticate.android.ui;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import sg.edu.nus.iss.phoenix.R;
 import sg.edu.nus.iss.phoenix.core.android.controller.ControlFactory;
@@ -23,6 +26,7 @@ public class LoginScreen extends AppCompatActivity {
     private TextView mSearchResultsTextView;
     private TextView mErrorMessageDisplay;
     private ProgressBar mLoadingIndicator;
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +39,6 @@ public class LoginScreen extends AppCompatActivity {
         mbtn_login = (Button) findViewById(R.id.btnLogin);
         mUsernameEditText = (EditText) findViewById(R.id.username);
         mPasswordEditText = (EditText) findViewById(R.id.password);
-
-        // mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
-        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
-        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
-
         // Set a click listener on Login Button
         mbtn_login.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the numbers category is clicked on.
@@ -50,6 +49,28 @@ public class LoginScreen extends AppCompatActivity {
             }
 
         });
+
+
+
+  /*      mVideoView = (VideoView) findViewById(R.id.videoView);
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.front0);
+
+        mVideoView.setVideoURI(uri);
+        mVideoView.start();
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
+
+    */
+
+
+
+
     }
 
     @Override
