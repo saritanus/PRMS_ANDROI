058ca51c50fb6ea5883d82b8e341b4b28131aed6
Raju Vimalraj <e0146859@u.nus.edu>
2017-09-27 18:32:47 +0800

    schedule layouts create, list, copy,modify

--- PRMS_ANDROID/app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
+++ PRMS_ANDROID/app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
@@ -2,7 +2,7 @@ package sg.edu.nus.iss.phoenix.core.android.delegate;
 
 public class DelegateHelper {
     // RESTful parameters.
-    public final static String PRMS_BASE_URL_AUTHENTICATE = "http://10.0.2.2:23964/phoenix/rest/Login/doLogin?";
-    public final static String PRMS_BASE_URL_RADIO_PROGRAM = "http://10.0.2.2:23964/phoenix/rest/radioprogram";
-    public final static String PRMS_BASE_URL_USER = "http://10.0.2.2:23964/phoenix/rest/user";
+    public final static String PRMS_BASE_URL_AUTHENTICATE = "http://172.17.252.72:23964/phoenix/rest/Login/doLogin?";
+    public final static String PRMS_BASE_URL_RADIO_PROGRAM = "http://172.17.252.72:23964/phoenix/rest/radioprogram";
+    public final static String PRMS_BASE_URL_USER = "http://172.17.252.72:23964/phoenix/rest/user";
 }
